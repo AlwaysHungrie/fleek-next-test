10ada08b0c107e2b6e6c7463c64e2466eb1d8898
Dhairya-CreatorOS <dhairya@creatoros.co>
2022-06-15 01:58:41 +0530

    route param test

---
+++ pages/[daoId]/create-dao.js
@@ -0,0 +1,9 @@
+const CreateDao = () => {
+  return (
+    <div>
+      <h1>Create Dao</h1>
+    </div>
+  );
+}
+
+export default CreateDao;
---
+++ pages/[daoId]/index.js
@@ -0,0 +1,9 @@
+const DaoHome = () => {
+  return (
+    <div>
+      <h1>Dao Home</h1>
+    </div>
+  );
+}
+
+export default DaoHome;
